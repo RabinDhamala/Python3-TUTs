5499a13fe2cb912cc39781df0a774370cc4e4aec
Rabin Dhamala <33034321+RabinDhamala@users.noreply.github.com>
2019-12-22 12:28:17 +0545

    Add files via upload

---
+++ PythonTut.py
@@ -0,0 +1,22 @@
+class Employee:
+    no_of_emps = 0
+    raise_amount = 1.04
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+        Employee.no_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def applyraise(self):
+        self.pay = int(self.raise_amount * self.pay)
+
+print(Employee.no_of_emps)
+
+emp1 = Employee('Hari', 'Khadka', 50000)
+emp2 = Employee('Nari', 'Chikari', 60000)
+
+print(Employee.no_of_emps)
